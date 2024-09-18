5500b8af1b0f57eacea9f5f502120c40ac9ae714
MarcosDEV <marcosvrsdevmail@gmail.com>
2024-09-18 00:32:40 -0300

    Correção do nome do usuário

--- site_rafaburguer/views.py
+++ site_rafaburguer/views.py
@@ -1,12 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from .models import ProdutoCadastroModel
+from .models import ProdutoCadastroModel, Categoria
 from .forms import ProdutoCadastroForm
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib import messages, auth
-from .models import ProdutoCadastroModel, Categoria
-
-# Create your views here.
 
 
 # Landing page site-cardápio
@@ -17,52 +14,36 @@ def index(request):
 # Index administrador
 @login_required(login_url="site_rafaburguer:tela_login")
 def index_administrador(request):
-    return render(
-        request, "global/index_administrador.html", {"username": request.user.username}
-    )
+    username = request.user.username
+    return render(request, "global/index_administrador.html", {"username": username})
 
 
 # Login/logout
 def login_view(request):
     form = AuthenticationForm(request)
-
     if request.method == "POST":
         form = AuthenticationForm(request, data=request.POST)
-
         if form.is_valid():
             user = form.get_user()
             auth.login(request, user)
             messages.success(request, "Logado com sucesso")
-            print(user)
-            return redirect(
-                "site_rafaburguer:index_administrador",
-            )
-
+            return redirect("site_rafaburguer:index_administrador")
         messages.error(request, "Login inválido")
-
-    return render(
-        request,
-        "global/tela_login.html",
-        {"form": form},
-    )
+    return render(request, "global/tela_login.html", {"form": form})
 
 
 @login_required(login_url="site_rafaburguer:tela_login")
 def logout_view(request):
     auth.logout(request)
-    return redirect(
-        "site_rafaburguer:tela_login",
-    )
+    return redirect("site_rafaburguer:tela_login")
 
 
 @login_required(login_url="site_rafaburguer:tela_login")
 def listar_produtos(request):
     username = request.user.username  # Obtém o nome de usuário do request
-
     produtos = ProdutoCadastroModel.objects.all()
-    categorias = Categoria.objects.all()  # Obtenha todas as categorias
+    categorias = Categoria.objects.all()
 
-    # Filtros de busca
     query_nome = request.GET.get("nome")
     query_categoria_id = request.GET.get("categoria")
     query_categoria_nome = None
@@ -73,14 +54,11 @@ def listar_produtos(request):
             query_categoria_nome = categoria.name
             produtos = produtos.filter(categoria__id=query_categoria_id)
         except Categoria.DoesNotExist:
-            query_categoria_id = (
-                None  # Se a categoria não existir, não filtrar por categoria
-            )
+            query_categoria_id = None
 
     if query_nome:
         produtos = produtos.filter(nome_do_produto__icontains=query_nome)
 
-    # Ordenar produtos por data de criação (mais novos primeiro)
     produtos = produtos.order_by("-data_de_criação")
 
     return render(
@@ -91,15 +69,14 @@ def listar_produtos(request):
             "categorias": categorias,
             "username": username,
             "query_nome": query_nome,
-            "query_categoria": query_categoria_nome,  # Passa o nome da categoria para o template
+            "query_categoria": query_categoria_nome,
         },
     )
 
 
-# Adicionar produto
 @login_required(login_url="site_rafaburguer:tela_login")
 def adicionar_produto(request):
-
+    username = request.user.username  # Obtém o nome de usuário do request
     if request.method == "POST":
         form = ProdutoCadastroForm(request.POST, request.FILES)
         if form.is_valid():
@@ -111,13 +88,13 @@ def adicionar_produto(request):
     return render(
         request,
         "global/partials/_adicionar_produto.html",
-        {"form": form},
+        {"form": form, "username": username},
     )
 
 
-# Atualizar produto
 @login_required(login_url="site_rafaburguer:tela_login")
 def atualizar_produto(request, id):
+    username = request.user.username  # Obtém o nome de usuário do request
     produto = get_object_or_404(ProdutoCadastroModel, id=id)
     if request.method == "POST":
         form = ProdutoCadastroForm(request.POST, request.FILES, instance=produto)
@@ -126,21 +103,19 @@ def atualizar_produto(request, id):
             messages.success(
                 request, f"Produto {produto.nome_do_produto} atualizado com sucesso!"
             )
-            return redirect(
-                "site_rafaburguer:listar_produtos",
-            )
+            return redirect("site_rafaburguer:listar_produtos")
     else:
         form = ProdutoCadastroForm(instance=produto)
     return render(
         request,
         "global/partials/_atualizar_produto.html",
-        {"form": form, "produto": produto},
+        {"form": form, "produto": produto, "username": username},
     )
 
 
-# Excluir produto
 @login_required(login_url="site_rafaburguer:tela_login")
 def excluir_produto(request, id):
+    username = request.user.username  # Obtém o nome de usuário do request
     produto = get_object_or_404(ProdutoCadastroModel, id=id)
     if request.method == "POST":
         produto.delete()
@@ -151,5 +126,5 @@ def excluir_produto(request, id):
     return render(
         request,
         "global/partials/_excluir_produto.html",
-        {"produto": produto},
+        {"produto": produto, "username": username},
     )
